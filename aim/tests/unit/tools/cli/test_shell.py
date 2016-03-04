2d5a8f7e122aee0c6a0fafede0e75ae8cd545d0d
Ivar Lazzaro <ivarlazzaro@gmail.com>
2016-03-03 18:16:09 -0800

    Add version command to CLI
    Also move around the CLI tests.

---
+++ aim/tests/unit/tools/cli/test_shell.py
@@ -0,0 +1,69 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import traceback
+
+from click import testing
+
+import aim
+from aim.tests import base
+from aim.tools.cli import shell
+
+
+class TestShell(base.BaseTestCase):
+    """Class for testing AIM """
+
+    def setUp(self):
+        super(TestShell, self).setUp()
+        self.runner = testing.CliRunner()
+        self.invoke = self.runner.invoke
+
+    def run_command(self, command, raises=False):
+        result = self.invoke(
+            shell.aim,
+            ['--config-file', self.test_conf_file] + command.split(' '))
+        if raises:
+            self._assert_command_exception(result)
+        else:
+            self._assert_command_no_exception(result)
+        return result
+
+    def _assert_command_no_exception(self, result):
+        self.assertFalse(
+            result.exception,
+            "Exception raised by AIM command, output:\n %s \n traceback %s" %
+            (result.output,
+             '\n'.join(traceback.format_tb(result.exc_info[-1]))))
+
+    def _assert_command_exception(self, result):
+        self.assertTrue(
+            result.exception, "Exception NOT raised by AIM command, "
+                              "output:\n %s" % result.output)
+
+    def _verify_db_manager_params(self, db_manager, num_called=1):
+        self.assertTrue(db_manager.called)
+        param = db_manager.call_args_list[0][0][0]
+        self.assertEqual(3, len(param))
+        self.assertTrue(
+            param['alembic_repo_path'].endswith(
+                'aim/db/migration/alembic_migrations/alembic'))
+        self.assertTrue(
+            param['alembic_ini_path'].endswith(
+                'aim/db/migration/alembic_migrations/alembic.ini'))
+        self.assertEqual('sqlite://', param['db_url'])
+
+
+class TestVersion(TestShell):
+
+    def test_version(self):
+        result = self.run_command('version')
+        self.assertEqual(aim.__version__ + '\n', result.output)
--- aim/tests/unit/tools/cli/test_shell_db_migration.py
+++ aim/tests/unit/tools/cli/test_shell_db_migration.py
@@ -10,56 +10,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import traceback
-
-from click import testing
 import mock
 
-from aim.tests import base
-from aim.tools.cli import shell
+from aim.tests.unit.tools.cli import test_shell as base
 
 
-class TestShell(base.BaseTestCase):
-    """Class for testing AIM """
-
-    def setUp(self):
-        super(TestShell, self).setUp()
-        self.runner = testing.CliRunner()
-        self.invoke = self.runner.invoke
-
-    def run_command(self, command, raises=False):
-        result = self.invoke(
-            shell.aim,
-            ['--config-file', self.test_conf_file] + command.split(' '))
-        if raises:
-            self._assert_command_exception(result)
-        else:
-            self._assert_command_no_exception(result)
-        return result
-
-    def _assert_command_no_exception(self, result):
-        self.assertFalse(
-            result.exception,
-            "Exception raised by AIM command, output:\n %s \n traceback %s" %
-            (result.output,
-             '\n'.join(traceback.format_tb(result.exc_info[-1]))))
-
-    def _assert_command_exception(self, result):
-        self.assertTrue(
-            result.exception, "Exception NOT raised by AIM command, "
-                              "output:\n %s" % result.output)
-
-    def _verify_db_manager_params(self, db_manager, num_called=1):
-        self.assertTrue(db_manager.called)
-        param = db_manager.call_args_list[0][0][0]
-        self.assertEqual(3, len(param))
-        self.assertTrue(
-            param['alembic_repo_path'].endswith(
-                'aim/db/migration/alembic_migrations/alembic'))
-        self.assertTrue(
-            param['alembic_ini_path'].endswith(
-                'aim/db/migration/alembic_migrations/alembic.ini'))
-        self.assertEqual('sqlite://', param['db_url'])
+class TestDBMigration(base.TestShell):
 
     def test_aim_db_migration(self):
         # aim db-migration
