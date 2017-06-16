noironetworks/aci-integration-module

commit ab6e0ce764974846ac305bac9168e24c45aa8226

K8S test environment compliance

aim/db/infra_model.py
@@ -57,36 +57,48 @@ class HostLinkManager(object):
     # things like "count" to exist in the result.
 
     def get_hostlink(self, host, ifname):
-        return self.aim_context.db_session.query(HostLink).filter_by(
-            host_name=host, interface_name=ifname).first()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        res = self.aim_context.store.query(db_type, infra.HostLink,
+                                           host_name=host,
+                                           interface_name=ifname)
+        return res[0] if res else None
 
     def get_hostlinks_for_host_switchport(self, host, swid, module, port):
-        return self.aim_context.db_session.query(HostLink).filter_by(
-            host_name=host, switch_id=swid, module=module, port=port).all()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        return self.aim_context.store.query(db_type, infra.HostLink,
+                                            host_name=host, switch_id=swid,
+                                            module=module, port=port)
 
     def get_hostlinks_for_switchport(self, swid, module, port):
-        return self.aim_context.db_session.query(HostLink).filter_by(
-            switch_id=swid, module=module, port=port).all()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        return self.aim_context.store.query(
+            db_type, infra.HostLink, switch_id=swid, module=module, port=port)
 
     def get_hostlinks(self):
-        return self.aim_context.db_session.query(HostLink).all()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        return self.aim_context.store.query(db_type, infra.HostLink)
 
     def get_hostlinks_for_host(self, host):
-        return self.aim_context.db_session.query(HostLink).filter_by(
-            host_name=host).all()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        return self.aim_context.store.query(db_type, infra.HostLink,
+                                            host_name=host)
 
     def get_switches(self):
-        return self.aim_context.db_session.query(HostLink.switch_id).distinct()
+        res = self.get_hostlinks()
+        return list(set([x.switch_id for x in res]))
 
     def get_modules_for_switch(self, swid):
-        return self.aim_context.db_session.query(
-            HostLink.module).filter_by(switch_id=swid).distinct()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        res = self.aim_context.store.query(db_type, infra.HostLink,
+                                           switch_id=swid)
+        return list(set([x.module for x in res]))
 
     def get_ports_for_switch_module(self, swid, module):
-        return self.aim_context.db_session.query(
-            HostLink.port).filter_by(switch_id=swid, module=module).distinct()
+        db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
+        res = self.aim_context.store.query(db_type, infra.HostLink,
+                                           switch_id=swid, module=module)
+        return list(set([x.port for x in res]))
 
     def get_switch_and_port_for_host(self, host):
-        return self.aim_context.db_session.query(
-            HostLink.switch_id, HostLink.module, HostLink.port).filter_by(
-                host_name=host).distinct()
+        res = self.get_hostlinks_for_host(host)
+        return list(set([(x.switch_id, x.module, x.port) for x in res]))

aim/tests/unit/test_infra_manager.py
@@ -62,13 +62,14 @@ class TestAimInfraManager(base.TestAimDBBase):
         self.assertEqual(hlinks[0].path, hlinks_mgr[0].path)
 
         hlinks = self.infra_mgr.get_modules_for_switch(swid)
-        self.assertEqual(hlinks[0][0], hlinks_mgr[0].module)
+        self.assertEqual(hlinks[0], hlinks_mgr[0].module)
 
         hlinks = self.infra_mgr.get_ports_for_switch_module(swid, module)
-        self.assertEqual(hlinks[0][0], hlinks_mgr[0].port)
+        self.assertEqual(hlinks[0], hlinks_mgr[0].port)
 
         hlinks = self.infra_mgr.get_switch_and_port_for_host(host)
-        self.assertEqual(hlinks[0][0], hlinks_mgr[0].switch_id)
+        self.assertEqual(hlinks[0], (hlinks_mgr[0].switch_id,
+                                     hlinks_mgr[0].module, hlinks_mgr[0].port))
 
         self.infra_mgr.delete_hostlink(host, ifname)
         # Idempotent

aim/agent/aid/event_services/event_service_base.py
@@ -35,8 +35,7 @@ class EventServiceBase(object):
 
     def __init__(self, conf):
         self.host = aim_cfg.CONF.aim.aim_service_identifier
-        self.session = api.get_session()
-        self.context = context.AimContext(self.session)
+        self.context = context.AimContext(store=api.get_store())
         self.conf_manager = aim_cfg.ConfigManager(self.context, self.host)
         # TODO(ivar): heartbeat for these services?
         self.sender = event_handler.EventSender()

aim/aim_lib/nat_strategy.py
@@ -279,7 +279,7 @@ class NatStrategyMixin(NatStrategy):
     def _create_l3out(self, ctx, l3out):
         """Create NAT EPG etc. in addition to creating L3Out."""
 
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             tenant = resource.Tenant(name=l3out.tenant_name)
             if not self.mgr.get(ctx, tenant):
                 self.mgr.create(ctx, tenant)
@@ -297,7 +297,7 @@ class NatStrategyMixin(NatStrategy):
     def _delete_l3out(self, ctx, l3out, delete_epg=True):
         """Delete NAT EPG etc. in addition to deleting L3Out."""
 
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             l3out_db = self.mgr.get(ctx, l3out)
             if l3out_db:
                 for en in self.mgr.find(ctx, resource.ExternalNetwork,
@@ -312,7 +312,7 @@ class NatStrategyMixin(NatStrategy):
                     self.mgr.delete(ctx, self._get_nat_vrf(ctx, l3out_db))
 
     def _create_ext_net(self, ctx, ext_net):
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             ext_net_db = self.mgr.get(ctx, ext_net)
             if not ext_net_db:
                 ext_net_db = self.mgr.create(ctx, ext_net)
@@ -324,7 +324,7 @@ class NatStrategyMixin(NatStrategy):
             return ext_net_db
 
     def _delete_ext_net(self, ctx, ext_net):
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             ext_net_db = self.mgr.get(ctx, ext_net)
             if ext_net_db:
                 self._manage_external_subnets(ctx, ext_net_db, [])
@@ -344,7 +344,7 @@ class NatStrategyMixin(NatStrategy):
                             external_network_name=ext_net.name)
         old_ext_subs = self.mgr.find(ctx, resource.ExternalSubnet,
                                      **ext_sub_attr)
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             for sub in old_ext_subs:
                 if sub.cidr in new_cidrs:
                     new_cidrs.remove(sub.cidr)
@@ -435,13 +435,13 @@ class NatStrategyMixin(NatStrategy):
 
     def _create_nat_epg(self, ctx, l3out):
         objs = self._get_nat_objects(ctx, l3out)
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             for r in objs:
                 if not self.mgr.get(ctx, r):
                     self.mgr.create(ctx, r)
 
     def _delete_nat_epg(self, ctx, l3out):
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             nat_bd = self._get_nat_bd(ctx, l3out)
             for sub in self.mgr.find(ctx, resource.Subnet,
                                      tenant_name=nat_bd.tenant_name,
@@ -503,7 +503,7 @@ class NoNatStrategy(NatStrategyMixin):
 
         Taking ownership allows us to modify the vrf_name of the L3Out.
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             l3out_db = self._create_l3out(ctx, l3outside)
             if l3out_db and l3out_db.monitored:
                 self.saved_l3out.push(ctx, l3out_db, 'monitored', True)
@@ -512,7 +512,7 @@ class NoNatStrategy(NatStrategyMixin):
 
     def delete_l3outside(self, ctx, l3outside):
         """Delete L3Out as normal, and relinquish ownership of the object."""
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             old_monitored = self.saved_l3out.pop(ctx, l3outside, 'monitored')
             if old_monitored is not None:
                 self.mgr.update(ctx, l3outside, monitored=old_monitored)
@@ -521,7 +521,7 @@ class NoNatStrategy(NatStrategyMixin):
     def delete_external_network(self, ctx, external_network):
         """Clean-up any connected VRFs before deleting the external network."""
 
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             ext_net = self.mgr.get(ctx, external_network)
             if not ext_net:
                 return
@@ -541,7 +541,7 @@ class NoNatStrategy(NatStrategyMixin):
         Locate BDs referring to the VRF, and include L3Outside
         in their l3out_names.
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             if not self._is_visible(vrf.tenant_name,
                                     external_network.tenant_name):
                 raise VrfNotVisibleFromExternalNetwork(
@@ -577,7 +577,7 @@ class NoNatStrategy(NatStrategyMixin):
         Locate BDs referring to the VRF, and exclude L3Outside
         from their l3out_names.
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             ext_net = self.mgr.get(ctx, external_network)
             if not ext_net:
                 return
@@ -665,7 +665,7 @@ class DistributedNatStrategy(NatStrategyMixin):
         """Delete external-network from main and cloned L3Outs.
 
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             # Delete specified external-network from all cloned L3Outs.
             # Delete external-network from main L3Out.
             l3out = self.mgr.get(ctx,
@@ -688,7 +688,7 @@ class DistributedNatStrategy(NatStrategyMixin):
         ext_net_db = self.mgr.get(ctx, external_network)
         if l3out and ext_net_db:
             clone_l3outs = self._find_l3out_clones(ctx, l3out)
-            with ctx.db_session.begin(subtransactions=True):
+            with ctx.store.begin(subtransactions=True):
                 for clone in clone_l3outs:
                     clone_ext_net = resource.ExternalNetwork(
                         tenant_name=clone.tenant_name,
@@ -709,7 +709,7 @@ class DistributedNatStrategy(NatStrategyMixin):
         Set vrf_name of shadow L3Outside to VRF.
 
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             return self._create_shadow(ctx, external_network, vrf)
 
     def disconnect_vrf(self, ctx, external_network, vrf):
@@ -720,7 +720,7 @@ class DistributedNatStrategy(NatStrategyMixin):
         there are no more ExternalNetworks in the shadow
         L3Outside.
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             self._delete_shadow(ctx, external_network, vrf)
 
     def _generate_l3out_name(self, l3outside, vrf):
@@ -762,7 +762,7 @@ class DistributedNatStrategy(NatStrategyMixin):
             _, nat_epg = self._get_nat_ap_epg(ctx, l3out)
             clone_ext_net.nat_epg_dn = nat_epg.dn
 
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             self.mgr.create(ctx, clone_l3out, overwrite=True)
             self.mgr.create(ctx, clone_ext_net, overwrite=True)
             cidrs = self.mgr.find(ctx, resource.ExternalSubnet,
@@ -787,7 +787,7 @@ class DistributedNatStrategy(NatStrategyMixin):
             l3out_name=clone_l3out.name,
             name=ext_net.name)
 
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             self._delete_ext_net(ctx, clone_ext_net)
             self._delete_unused_l3out(ctx, clone_l3out)
 
@@ -822,7 +822,7 @@ class EdgeNatStrategy(DistributedNatStrategy):
         Set vrf_name of shadow L3Outside to VRF.
 
         """
-        with ctx.db_session.begin(subtransactions=True):
+        with ctx.store.begin(subtransactions=True):
             return self._create_shadow(ctx, external_network, vrf,
                                        with_nat_epg=False)
 

aim/aim_store.py
@@ -47,22 +47,26 @@ def _begin(**kwargs):
 class AimStore(object):
     """Interface to backend persistence for AIM resources."""
 
+    _features = []
+
     def __init__(self):
         self._hashtree_db_listener = ht_db_l.HashTreeDbListener(
             aim_manager.AimManager(), self)
         self._update_listeners = []
 
+    def __getattr__(self, item):
+        if item.startswith('supports_'):
+            return item.replace('supports_', '') in self.features
+        else:
+            raise AttributeError(item)
+
     @property
-    def supports_hooks(self):
-        return True
+    def features(self):
+        return self._features
 
     @property
     def current_timestamp(self):
         return None
-
-    @property
-    def supports_foreign_keys(self):
-        return False
 
     def begin(self, **kwargs):
         # Begin transaction of updates, if applicable.
@@ -157,6 +161,8 @@ class AimStore(object):
 
 class SqlAlchemyStore(AimStore):
 
+    _features = ['foreign_keys', 'timestamp', 'hooks']
+
     # Dict mapping AIM resources to DB model objects
     db_model_map = {api_res.BridgeDomain: models.BridgeDomain,
                     api_res.Agent: agent_model.Agent,
@@ -198,6 +204,10 @@ class SqlAlchemyStore(AimStore):
         for k, v in self.db_model_map.iteritems():
             self.resource_map[v] = k
         self.register_update_listener(self._hashtree_db_listener.on_commit)
+
+    @property
+    def name(self):
+        return 'SQLAlchemy'
 
     @property
     def current_timestamp(self):
@@ -272,6 +282,10 @@ class K8sStore(AimStore):
         super(K8sStore, self).__init__()
         self.klient = api_v1.AciContainersV1(config_file=config_file)
         self.namespace = namespace or api_v1.K8S_DEFAULT_NAMESPACE
+
+    @property
+    def name(self):
+        return 'Kubernetes'
 
     @property
     def supports_hooks(self):

aim/context.py
@@ -28,4 +28,7 @@ class AimContext(object):
     # For backwards compatibility
     @property
     def db_session(self):
-        return self.store.db_session
+        try:
+            return self.store.db_session
+        except AttributeError:
+            return None

aim/tests/base.py
@@ -22,6 +22,7 @@ from oslo_utils import uuidutils
 from oslotest import base
 from sqlalchemy.orm import sessionmaker as sa_sessionmaker
 
+from aim.agent.aid.universes.aci import aci_universe
 from aim import aim_manager
 from aim import aim_store
 from aim.api import resource
@@ -61,6 +62,17 @@ def resource_equal(self, other):
                 sort_if_list(getattr(other, attr, None))):
             return False
     return True
+
+
+def requires(requirements):
+    def wrap(func):
+        def inner(self, *args, **kwargs):
+            diff = set(requirements) - set(self.ctx.store.features)
+            if diff:
+                self.skipTest("Store %s doesn't support required "
+                              "features: %s" % (self.ctx.store.name, diff))
+        return inner
+    return wrap
 
 
 class BaseTestCase(base.BaseTestCase):
@@ -128,6 +140,8 @@ class TestAimDBBase(BaseTestCase):
     def setUp(self):
         super(TestAimDBBase, self).setUp()
         self.test_id = uuidutils.generate_uuid()
+        aim_cfg.OPTION_SUBSCRIBER_MANAGER = None
+        aci_universe.ws_context = None
         if not os.environ.get(K8S_STORE_VENV):
             CONF.set_override('aim_store', 'sql', 'aim')
             self.engine = api.get_engine()
@@ -149,7 +163,7 @@ class TestAimDBBase(BaseTestCase):
         else:
             CONF.set_override('aim_store', 'k8s', 'aim')
             CONF.set_override('k8s_namespace', self.test_id, 'aim_k8s')
-            aim_store.K8sStore._post_deleted = _k8s_post_delete
+            aim_store.K8sStore._post_delete = _k8s_post_delete
             aim_store.K8sStore._post_create = _k8s_post_create
             self.addCleanup(self._cleanup_namespace, self.test_id)
 

aim/tests/unit/agent/test_agent.py
@@ -153,6 +153,7 @@ class TestAgent(base.TestAimDBBase, test_aci_tenant.TestAciClientMixin):
         self.assertEqual(1, len(agents))
         self.assertEqual('aid-h1', agents[0].id)
 
+    @base.requires(['timestamp'])
     def test_send_heartbeat(self):
         agent = self._create_agent()
         current_tstamp = agent.agent.heartbeat_timestamp
@@ -534,6 +535,7 @@ class TestAgent(base.TestAimDBBase, test_aci_tenant.TestAciClientMixin):
         self._assert_universe_sync(desired_monitor, current_monitor)
         self._assert_reset_consistency(tenant_name)
 
+    @base.requires('foreign_keys')
     def test_monitored_tree_fk_semantics(self):
         agent = self._create_agent()
 
@@ -713,11 +715,14 @@ class TestAgent(base.TestAimDBBase, test_aci_tenant.TestAciClientMixin):
         self._observe_aci_events(current_config)
         agent._daemon_loop()
         # Verify sync status of BD
-        bd = self.aim_manager.get(self.ctx, bd)
-        bd_status = self.aim_manager.get_status(self.ctx, bd)
-        self.assertEqual(aim_status.AciStatus.SYNC_FAILED,
-                         bd_status.sync_status)
-        self.assertNotEqual('', bd_status.sync_message)
+        if self.ctx.store.supports_foreign_keys:
+            bd = self.aim_manager.get(self.ctx, bd)
+            bd_status = self.aim_manager.get_status(self.ctx, bd)
+            self.assertEqual(aim_status.AciStatus.SYNC_FAILED,
+                             bd_status.sync_status)
+            self.assertNotEqual('', bd_status.sync_message)
+        else:
+            self.assertIsNone(self.aim_manager.get(self.ctx, bd))
         # Same for subnet
         sub = self.aim_manager.get(self.ctx, sub)
         sub_status = self.aim_manager.get_status(self.ctx, sub)
@@ -1002,6 +1007,7 @@ class TestAgent(base.TestAimDBBase, test_aci_tenant.TestAciClientMixin):
         status = self.aim_manager.get_status(self.ctx, epg)
         self.assertEqual(status.SYNCED, status.sync_status)
 
+    @base.requires('foreign_keys')
     def test_no_nat_strategy_lifecycle(self):
         """Test ownership changes made by no-NAT strategy."""
         agent = self._create_agent()

commit e12ad2caeb6abffefb2d6048a00fade453a5f37a

Fix HostLinkManager return values
Some functions are expected to return tuples instead of
scalar values. Bug was introduced during the refactor
in commit ab6e0ce764974846ac305bac9168e24c45aa8226.

Signed-off-by: Amit Bose <amitbose@gmail.com>

aim/db/infra_model.py
@@ -85,19 +85,19 @@ class HostLinkManager(object):
 
     def get_switches(self):
         res = self.get_hostlinks()
-        return list(set([x.switch_id for x in res]))
+        return list(set([(x.switch_id,) for x in res]))
 
     def get_modules_for_switch(self, swid):
         db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
         res = self.aim_context.store.query(db_type, infra.HostLink,
                                            switch_id=swid)
-        return list(set([x.module for x in res]))
+        return list(set([(x.module,) for x in res]))
 
     def get_ports_for_switch_module(self, swid, module):
         db_type = self.aim_context.store.resource_to_db_type(infra.HostLink)
         res = self.aim_context.store.query(db_type, infra.HostLink,
                                            switch_id=swid, module=module)
-        return list(set([x.port for x in res]))
+        return list(set([(x.port,) for x in res]))
 
     def get_switch_and_port_for_host(self, host):
         res = self.get_hostlinks_for_host(host)

aim/tests/unit/test_infra_manager.py
@@ -62,10 +62,10 @@ class TestAimInfraManager(base.TestAimDBBase):
         self.assertEqual(hlinks[0].path, hlinks_mgr[0].path)
 
         hlinks = self.infra_mgr.get_modules_for_switch(swid)
-        self.assertEqual(hlinks[0], hlinks_mgr[0].module)
+        self.assertEqual(hlinks[0], (hlinks_mgr[0].module,))
 
         hlinks = self.infra_mgr.get_ports_for_switch_module(swid, module)
-        self.assertEqual(hlinks[0], hlinks_mgr[0].port)
+        self.assertEqual(hlinks[0], (hlinks_mgr[0].port,))
 
         hlinks = self.infra_mgr.get_switch_and_port_for_host(host)
         self.assertEqual(hlinks[0], (hlinks_mgr[0].switch_id,