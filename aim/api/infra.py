db4f4382ddd1f3f92373d76c09b5c3b72ef1f1ec
Thomas Bachman <tbachman@yahoo.com>
2017-10-28 00:23:15 +0000

    Generalize HostDomainMapping table
    The vmm_domain_name column is changed to just domain_name and made
    a primary key, while the physical_domain_name is dropped in favor
    of a domain_type, which is an enum for the supported Domain types.
    This is needed in order to allow multiple entries with the same
    hostname in the table, which can be used to support a catch-all
    or "wildcard" of all hosts for multiple VMM domains.

--- aim/api/infra.py
+++ aim/api/infra.py
@@ -76,15 +76,16 @@ class HostDomainMapping(resource.ResourceBase):
     """host to VMM and phys-dom mapping"""
 
     identity_attributes = t.identity(
-        ('host_name', t.string(128)))
-    other_attributes = t.other(
-        ('vmm_domain_name', t.string(64)),
-        ('physical_domain_name', t.string(64)))
+        ('host_name', t.string(128)),
+        ('domain_name', t.string(64)),
+        ('domain_type', t.enum('PhysDom',
+                               'OpenStack',
+                               'Kubernetes',
+                               'VMware')))
+    other_attributes = t.other()
 
     def __init__(self, **kwargs):
-        super(HostDomainMapping, self).__init__({'vmm_domain_name': '',
-                                                 'physical_domain_name': ''},
-                                                **kwargs)
+        super(HostDomainMapping, self).__init__({}, **kwargs)
 
 
 class HostLinkNetworkLabel(resource.ResourceBase):
