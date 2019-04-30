4fb4cabf8a7e0cbb9546b892d4a3867a19fa0d04
Kent Wu <wu@noironetworks.com>
2019-04-29 17:31:03 -0700

    Don't delete the tenant prematurely
    Make sure aci_universe has populated the tenant in its tree before
    aim_universe can go ahead to remove it. This is to prevent a situation
    especially during tempest tests that a tenant is being created and
    deleted rapidly. What happens there is that before the tenant thread
    can pick up the tenant creation event from APIC then update the
    aci_universe tree accordingly, that tenant is already deleted in AIM.
    Then the sanity test in aim_universe finalize_deletion_candidates()
    will still pass because at that point, both aim and aci universe don't
    have that tenant in its tree.

--- aim/common/hashtree/structured_tree.py
+++ aim/common/hashtree/structured_tree.py
@@ -178,9 +178,9 @@ class StructuredHashTree(base.ComparableCollection):
     tree.pop(('tn-tenant', 'bd-bridge3'))
     """
 
-    __slots__ = ['root', 'root_key']
+    __slots__ = ['root', 'root_key', 'has_populated']
 
-    def __init__(self, root=None, root_key=None):
+    def __init__(self, root=None, root_key=None, has_populated=False):
         """Initialize a Structured Hash Tree.
 
         Initial data can be passed to initialize the tree
@@ -191,6 +191,7 @@ class StructuredHashTree(base.ComparableCollection):
         if self.root:
             # Ignore the value passed in the constructor
             self.root_key = self.root.key
+        self.has_populated = has_populated
 
     @property
     def root_full_hash(self):
@@ -202,10 +203,12 @@ class StructuredHashTree(base.ComparableCollection):
             return None
 
     @staticmethod
-    def from_string(string, root_key=None):
+    def from_string(string, root_key=None, has_populated=False):
         to_dict = utils.json_loads(string)
-        return (StructuredHashTree(StructuredHashTree._build_tree(to_dict)) if
-                to_dict else StructuredHashTree(root_key=root_key))
+        return (StructuredHashTree(StructuredHashTree._build_tree(to_dict),
+                                   has_populated=has_populated) if
+                to_dict else StructuredHashTree(root_key=root_key,
+                                                has_populated=has_populated))
 
     @staticmethod
     def _build_tree(root_dict):
@@ -233,6 +236,7 @@ class StructuredHashTree(base.ComparableCollection):
             self.root = StructuredTreeNode(
                 (key[0],), self._hash_attributes(key=(key[0],), _dummy=True))
             self.root_key = self.root.key
+            self.has_populated = True
         else:
             # With the first element of the key, verify that this is not an
             # attempt of creating a hydra (tree with multiple roots)
