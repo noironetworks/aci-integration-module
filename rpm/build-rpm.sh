c97e07e7f4ed0253b34f163e06f62449de544112
Thomas Bachman <tbachman@yahoo.com>
2020-09-06 11:34:37 +0000

    Support python2 packages for RHEL7/8
    Update package names to support packages for both RHEL7 and RHEL8.

--- rpm/build-rpm.sh
+++ rpm/build-rpm.sh
@@ -12,9 +12,9 @@ if [ -z $SPEC_FILE_IN ]; then
 fi
 BUILD_DIR=${BUILD_DIR:-`pwd`/rpmbuild}
 mkdir -p $BUILD_DIR/BUILD $BUILD_DIR/SOURCES $BUILD_DIR/SPECS $BUILD_DIR/RPMS $BUILD_DIR/SRPMS
-NAME=`python setup.py --name`
+NAME=`python2 setup.py --name`
 RELEASE=${RELEASE:-1}
-VERSION_PY=`python setup.py --version`
+VERSION_PY=`python2 setup.py --version`
 VERSION=`echo $VERSION_PY | sed -nre 's,([^\.]+.[^\.]+.[^\.]+)((\.)(0[^\.]+))?((\.)(dev.*))?,\1 \4 \7,p' | sed -re 's/ *$//g' | sed -re 's/ +/~/g'`
 SPEC_FILE=${SPEC_FILE_IN/.in/}
 SPEC_FILE=${SPEC_FILE/rpm\//}
@@ -22,6 +22,6 @@ sed -e "s/@VERSION@/$VERSION/" \
     -e "s/@VERSION_PY@/$VERSION_PY/" \
     -e "s/@RELEASE@/$RELEASE/" \
     $SPEC_FILE_IN > $BUILD_DIR/SPECS/$SPEC_FILE
-python setup.py sdist --dist-dir $BUILD_DIR/SOURCES
+python2 setup.py sdist --dist-dir $BUILD_DIR/SOURCES
 mv $BUILD_DIR/SOURCES/$NAME-$VERSION_PY.tar.gz $BUILD_DIR/SOURCES/$NAME-$VERSION.tar.gz
 rpmbuild --clean -ba --define "_topdir $BUILD_DIR" $BUILD_DIR/SPECS/$SPEC_FILE
